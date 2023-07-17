0e90c58b411f08ef91bfbb36aff7e37c95b751ca
shivbatra <shivbatra26@gmail.com>
2023-07-17 15:28:18 +0530

    Test Class commit

--- src/test/java/com/jenkins/demo/SpringJenkinsDemoApplicationTests.java
+++ src/test/java/com/jenkins/demo/SpringJenkinsDemoApplicationTests.java
@@ -3,13 +3,18 @@ package com.jenkins.demo;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringJenkinsDemoApplicationTests {
 
+	private static Logger logger = LoggerFactory.getLogger(SpringJenkinsDemoApplication.class);
+	
 	@Test
 	void contextLoads() {
+		logger.info("-----------------------Test Case Executing -----------------------------------------");
 		assertTrue(true);
 	}
 
